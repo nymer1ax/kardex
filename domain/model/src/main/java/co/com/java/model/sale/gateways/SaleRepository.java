cbb8faf4b45c30fdb9209ca6ebd0aaaa63937af3
Ralph <devnymeria@gmail.com>
2023-06-08 22:12:20 -0500

    get all sales

--- domain/model/src/main/java/co/com/java/model/sale/gateways/SaleRepository.java
+++ domain/model/src/main/java/co/com/java/model/sale/gateways/SaleRepository.java
@@ -2,6 +2,10 @@ package co.com.java.model.sale.gateways;
 
 import co.com.java.model.sale.Sale;
 
+import java.util.List;
+
 public interface SaleRepository {
     void saveSale(Sale sale);
+
+    List<Sale> getAllSales();
 }
--- domain/usecase/src/main/java/co/com/java/usecase/getsales/GetSalesUseCase.java
+++ domain/usecase/src/main/java/co/com/java/usecase/getsales/GetSalesUseCase.java
@@ -1,6 +1,21 @@
 package co.com.java.usecase.getsales;
 
+import co.com.java.model.inventory.gateways.InventoryRepository;
+import co.com.java.model.movements.gateways.MovementsRepository;
+import co.com.java.model.product.gateways.ProductRepository;
+import co.com.java.model.sale.Sale;
+import co.com.java.model.sale.gateways.SaleRepository;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 @RequiredArgsConstructor
 public class GetSalesUseCase {
+
+    private final SaleRepository saleRepository;
+
+    public List<Sale> getSales(){
+        return saleRepository.getAllSales();
+    }
+
 }
